08407f8a329936de1b9b3b3746ae07a6370338a4
ColorfulBanana <therealrebuilder@gmail.com>
2016-07-26 19:46:24 +0200

    Use mistune markdown parser to render markdown, missing block_quote, block_html, hrule, table, table_row and table_cell

--- pythonwars/util.py
+++ pythonwars/util.py
@@ -1,28 +1,80 @@
 #!/usr/bin/env python3
-import collections
-
+import colorama
 import inflection
-import parsley
+import mistune
+import pygments
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import TerminalFormatter
 
 __all__ = ["_AttributesFromJson", "markdown_to_ansi"]
 
-GRAMMAR_FUNCTIONS = {
-    "bold": "\033[1m{}\033[0m".format,
-    "italic": "\033[3m{}\033[0m".format,
-    "header": "\033[4m{}\033[0m".format,
-    "code": "\033[32m{}\033[0m".format,
-    # TODO: Add some sort of highlighting
-    "multiline_code": lambda c, _: GRAMMAR_FUNCTIONS["code"](c)
-}
-MARKDOWN_GRAMMAR = parsley.makeGrammar("""
-nonspecial = anything:x ?(x not in '*_`') -> x
-bold = '*' '*' <nonspecial+>:x '*' '*' -> bold(x)
-italic = '_' <nonspecial+>:x '_' -> italic(x)
-header = ('#' <anything+>:x | <anything+>:y '\n' '='+) -> header(x or y)
-code = '`' <nonspecial+>:x '`' -> code(x)
-multiline_code = '`'{3} <anything*>:lang '\n' <anything*>:code '\n' '`'{3} -> multiline_code(code, lang)
-text = (header | bold | italic | code | multiline_code | <nonspecial+>)+
-""", GRAMMAR_FUNCTIONS)
+
+class AnsiRenderer(mistune.Renderer):
+    def block_code(self, code, language=None):
+        if not language:
+            return "\n<pre><code>{}</code></pre>\n".format(mistune.escape(code))
+        lexer = get_lexer_by_name(language, stripall=True)
+        return pygments.highlight(code, lexer, TerminalFormatter())
+
+    # MISSING: block_quote(text), block_html(html)
+
+    def header(self, text, level, raw=None):
+        # TODO: Care about level?
+        return colorama.Style.BRIGHT + text + colorama.Style.RESET_ALL + "\n"
+
+    # MISSING: hrule()
+
+    def list(self, body, ordered=True):
+        items = body.splitlines()
+        if ordered:
+            return "\n".join("{}. {}".format(i, x)
+                             for i, x in enumerate(items, start=1)) + "\n"
+        else:
+            return "\n".join("* {}".format(x) for x in items) + "\n"
+
+    def list_item(self, text):
+        return text + "\n"
+
+    def paragraph(self, text):
+        return text + "\n"
+
+    # MISSING: table(header, body), table_row(content), table_cell(content,
+    # **flags)
+
+    def autolink(self, link, is_email=False):
+        return "<{}>".format(link)
+
+    def codespan(self, text):
+        return colorama.Fore.GREEN + text + colorama.Style.RESET_ALL + "\n"
+
+    def double_emphasis(self, text):
+        return colorama.Fore.BRIGHT + text + colorama.Style.RESET_ALL + "\n"
+
+    def emphasis(self, text):
+        return colorama.Fore.BRIGHT + text + colorama.Style.RESET_ALL + "\n"
+
+    def image(self, src, title, alt_text):
+        # XXX: What should we do here?
+        return colorama.Fore.RED + alt_text + colorama.Style.RESET_ALL + "\n"
+
+    def linebreak(self):
+        return "\n"
+
+    def newline(self):
+        return "\n"
+
+    def link(self, link, title, content):
+        # XXX: What should we do here?
+        return "{} ({})".format(content, link) + "\n"
+
+    def strikethrough(self, text):
+        return colorama.Style.STRIKE + text + colorama.Style.RESET_ALL + "\n"
+
+    def text(self, text):
+        return text
+
+    def inline_html(self, text):
+        return self.code(text)
 
 
 class _AttributesFromJson(object):
@@ -39,4 +91,4 @@ class _AttributesFromJson(object):
 
 
 def markdown_to_ansi(md_text):
-    return "".join(MARKDOWN_GRAMMAR(md_text).text())
+    return mistune.markdown(md_text, renderer=AnsiRenderer())
