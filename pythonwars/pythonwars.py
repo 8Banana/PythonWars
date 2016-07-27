38cd2e1a067142f4beae3e625350cf64eaab0920
SquishyStrawberry <SquishyStrawberry@users.noreply.github.com>
2016-07-28 01:02:36 +0200

    Use general JsonDict instead of specialized classes (#4)
    * Remove `AttributesFromJson` and add `JsonDict`
    
    WARNING: This commit is broken!
    
    * Actually use the JsonDict in the code
    
    * Remove useless code
    
    * Move JsonDict to its own file and add __setattr__
    
    * Update requirements.txt and remove JsonDict

--- pythonwars/pythonwars.py
+++ pythonwars/pythonwars.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 import requests
 
-from .user import User
-from .codechallenge import TrainingCodeChallenge, CodeChallengeInfo
-
 
 _GET_USER_URL = "https://codewars.com/api/v1/users/{}"
 _GET_CODE_CHALLENGE_URL = "https://www.codewars.com/api/v1/code-challenges/{}"
@@ -23,34 +20,28 @@ class CodeWars:
                 "Authorization": api_key,
             })
 
-    def _request_json(self, url, cls=None, **kwargs):
+    def _request_json(self, url, **kwargs):
         # If you pass data, `.get` automatically becomes `.post`
         response = self.session.get(url, **kwargs)
         response.raise_for_status()
-        if cls is None:
-            return response.json()
-        else:
-            return cls(response.json())
+        return {inflection.underscore(k): v
+                for k, v in response.json().items()}
 
     def get_user(self, id_or_username):
-        return self._request_json(_GET_USER_URL.format(id_or_username), User)
+        return self._request_json(_GET_USER_URL.format(id_or_username))
 
     def get_code_challenge(self, id_or_slug):
-        return self._request_json(_GET_CODE_CHALLENGE_URL.format(id_or_slug),
-                                  CodeChallengeInfo)
+        return self._request_json(_GET_CODE_CHALLENGE_URL.format(id_or_slug))
 
     def train_next_code_challenge(self, language, strategy="default", peek=False):
         return self._request_json(_TRAIN_NEXT_CODE_CHALLENGE_URL.format(language),
-                                  TrainingCodeChallenge,
                                   data={"strategy": strategy, "peek": peek})
 
     def train_code_challenge(self, id_or_slug, language):
-        return self._request_json(_TRAIN_CODE_CHALLENGE_URL.format(id_or_slug, language),
-                                  TrainingCodeChallenge)
+        return self._request_json(_TRAIN_CODE_CHALLENGE_URL.format(id_or_slug, language))
 
     def attempt_solution(self, project_id, solution_id, code, output_format="html"):
         return self._request_json(_ATTEMPT_SOLUTION_URL.format(project_id, solution_id),
-                                  None,
                                   data={"code": code,
                                         "output_format": output_format})
 
