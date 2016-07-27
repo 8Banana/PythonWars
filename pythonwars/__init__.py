af4368af88e5812c6684457edb1b937646947724
ColorfulBanana <therealrebuilder@gmail.com>
2016-07-27 23:41:58 +0200

    Actually use the JsonDict in the code

--- pythonwars/__init__.py
+++ pythonwars/__init__.py
@@ -1,6 +1,3 @@
 #!/usr/bin/env python3
 from .pythonwars import CodeWars
-from .user import User, CodeChallengeRank, UserLanguageRank
-from .codechallenge import CodeChallengeInfo, TrainingCodeChallenge
-
-__version__ = "0.2.0"
+from .util import JsonDict
