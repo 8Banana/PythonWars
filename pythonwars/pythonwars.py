f4f3e4acd3ff8dc39fc4b4dddb1ea3967dd6244c
ColorfulBanana <therealrebuilder@gmail.com>
2016-07-27 21:30:03 +0200

    Refactor code to remove redundant redundancy
    Added `self._request_json` method that handles all the HTTP requests, json
    parsing, etc...

--- pythonwars/pythonwars.py
+++ pythonwars/pythonwars.py
@@ -12,6 +12,7 @@ _TRAIN_CODE_CHALLENGE_URL = "https://www.codewars.com/api/v1/code-challenges/{}/
 _ATTEMPT_SOLUTION_URL = "https://www.codewars.com/api/v1/code-challenges/projects/:project_id/solutions/:solution_id/attempt"
 _FINALIZE_SOLUTION_URL = "https://www.codewars.com/api/v1/code-challenges/projects/{}/solutions/{}/finalize"
 
+
 class CodeWars:
 
     def __init__(self, api_key=None):
@@ -22,46 +23,36 @@ class CodeWars:
                 "Authorization": api_key,
             })
 
-    def get_user(self, id_or_username):
-        url = _GET_USER_URL.format(id_or_username)
-        response = self.session.get(url)
+    def _request_json(self, url, cls=None, **kwargs):
+        # If you pass data, `.get` automatically becomes `.post`
+        response = self.session.get(url, **kwargs)
         response.raise_for_status()
-        return User(response.json())
+        if cls is None:
+            return response.json()
+        else:
+            return cls(response.json())
+
+    def get_user(self, id_or_username):
+        return self._request_json(_GET_USER_URL.format(id_or_username), User)
 
     def get_code_challenge(self, id_or_slug):
-        url = _GET_CODE_CHALLENGE_URL.format(id_or_slug)
-        response = self.session.get(url)
-        response.raise_for_status()
-        return CodeChallengeInfo(response.json())
+        return self._request_json(_GET_CODE_CHALLENGE_URL.format(id_or_slug),
+                                  CodeChallengeInfo)
 
     def train_next_code_challenge(self, language, strategy="default", peek=False):
-        url = _TRAIN_NEXT_CODE_CHALLENGE_URL.format(language)
-        post_data = {
-            "strategy": strategy,
-            "peek": peek,
-        }
-        response = self.session.post(url, data=post_data)
-        response.raise_for_status()
-        return TrainingCodeChallenge(response.json())
+        return self._request_json(_TRAIN_NEXT_CODE_CHALLENGE_URL.format(language),
+                                  TrainingCodeChallenge,
+                                  data={"strategy": strategy, "peek": peek})
 
     def train_code_challenge(self, id_or_slug, language):
-        url = _TRAIN_CODE_CHALLENGE_URL.format(id_or_slug, language)
-        response = self.session.post(url)
-        response.raise_for_status()
-        return TrainingCodeChallenge(response.json())
+        return self._request_json(_TRAIN_CODE_CHALLENGE_URL.format(id_or_slug, language),
+                                  TrainingCodeChallenge)
 
     def attempt_solution(self, project_id, solution_id, code, output_format="html"):
-        url = _ATTEMPT_SOLUTION_URL.format(project_id, solution_id)
-        post_data = {
-            "code": code,
-            "output_format": output_format,
-        }
-        response = self.session.post(url, data=post_data)
-        response.raise_for_status()
-        return response.json()
+        return self._request_json(_ATTEMPT_SOLUTION_URL.format(project_id, solution_id),
+                                  None,
+                                  data={"code": code,
+                                        "output_format": output_format})
 
     def finalize_solution(self, project_id, solution_id):
-        url = _FINALIZE_SOLUTION_URL.format(project_id, solution_id)
-        response = self.session.post(url)
-        response.raise_for_status()
-        return response.json()
+        return self._request_json(_FINALIZE_SOLUTION_URL.format(project_id, solution_id))
