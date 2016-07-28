9eb07c5f2fc7f1dc33ad9e92c051e64e85cbb70c
Akuli <akuviljanen17@gmail.com>
2016-07-28 11:18:47 +0300

    Don't fear mixedCase

--- pythonwars/pythonwars.py
+++ pythonwars/pythonwars.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-import inflection
 import requests
 
 
@@ -24,31 +22,30 @@ class CodeWars:
     def _request_json(self, method, url, **kwargs):
         response = self.session.request(method, url, **kwargs)
         response.raise_for_status()
-        return {inflection.underscore(k): v
-                for k, v in response.json().items()}
+        return response.json().items()
 
     def get_user(self, id_or_username):
-        return self._request_json("get", _GET_USER_URL.format(id_or_username))
+        return self._request_json(
+            "get", _GET_USER_URL.format(id_or_username))
 
     def get_code_challenge(self, id_or_slug):
-        return self._request_json("get",
-                                  _GET_CODE_CHALLENGE_URL.format(id_or_slug))
+        return self._request_json(
+            "get", _GET_CODE_CHALLENGE_URL.format(id_or_slug))
 
     def train_next_code_challenge(self, language, strategy="default", peek=False):
-        return self._request_json("post",
-                                  _TRAIN_NEXT_CODE_CHALLENGE_URL.format(language),
-                                  data={"strategy": strategy, "peek": peek})
+        return self._request_json(
+            "post", _TRAIN_NEXT_CODE_CHALLENGE_URL.format(language),
+            data={"strategy": strategy, "peek": peek})
 
     def train_code_challenge(self, id_or_slug, language):
-        return self._request_json("post",
-                                  _TRAIN_CODE_CHALLENGE_URL.format(id_or_slug, language))
+        return self._request_json(
+            "post", _TRAIN_CODE_CHALLENGE_URL.format(id_or_slug, language))
 
     def attempt_solution(self, project_id, solution_id, code, output_format="html"):
-        return self._request_json("post",
-                                  _ATTEMPT_SOLUTION_URL.format(project_id, solution_id),
-                                  data={"code": code,
-                                        "output_format": output_format})
+        return self._request_json(
+            "post", _ATTEMPT_SOLUTION_URL.format(project_id, solution_id),
+            data={"code": code, "output_format": output_format})
 
     def finalize_solution(self, project_id, solution_id):
-        return self._request_json("post",
-                                  _FINALIZE_SOLUTION_URL.format(project_id, solution_id))
+        return self._request_json(
+            "post", _FINALIZE_SOLUTION_URL.format(project_id, solution_id))
