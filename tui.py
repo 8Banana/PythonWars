45213a083b5b3ecb12ce28f8ab56ccfc29e0c40b
theelous3 <theegrandmaster@gmail.com>
2016-07-29 18:24:57 +0100

    relative position rendering

--- tui.py
+++ tui.py
@@ -12,9 +12,11 @@ from pythonwars.pythonwars import CodeWars
 class Tui:
     def __init__(self):
         self.code_wars = None
+        self.scr_size = None
 
     def main(self, curse_main):
-
+        self.scr_size = ((curse_main.getmaxyx()[0] // 2), 
+                        (curse_main.getmaxyx()[1] // 2))
         curses.cbreak()
         self.login_screen(curse_main)
         curse_main.clear()
@@ -22,22 +24,25 @@ class Tui:
 
     def login_screen(self, login_scr):
         
-        login_scr.addstr('-'*47 + 'Welcome to Codewars!' + '-'*47)
+        login_scr.addstr('-'*int(self.scr_size[1] - 11) + 
+                        'Welcome to CodeWars!' + 
+                        '-'*int(self.scr_size[1] - 10))
         login_scr.refresh()
         time.sleep(1)
 
-        login_scr.addstr(54, 43, 'Please enter your API token:')
+        login_scr.addstr(int(self.scr_size[0] + 5), 
+                        int(self.scr_size[1] - 14), 
+                        'Please enter your API token:')
         login_scr.refresh()
         curses.echo()
         time.sleep(1)
-
-        logo_thrd = Thread(self.logo_print(login_scr))
-        logo_thrd.start()
-        
+        if self.scr_size[0] > 28:
+            logo_thrd = Thread(self.logo_print(login_scr))
+            logo_thrd.start()
         user_api = ''
-        
         getch_position = [0, 0]
-        input_scr = curses.newwin(1,20, 55,47)
+        input_scr = curses.newwin(1,21, int(self.scr_size[0] + 6), 
+                                        int(self.scr_size[1] - 10))
         input_scr.keypad(True)
         input_scr.nodelay(True)
 
@@ -62,7 +67,9 @@ class Tui:
         input_scr.nodelay(False)
         self.code_wars = CodeWars(user_api)
 
-        login_scr.addstr(56, 45, 'Press enter to confirm.')
+        login_scr.addstr(int(self.scr_size[0] + 8), 
+                        int(self.scr_size[1] -12), 
+                        'Press enter to confirm.')
         login_scr.getch()
 
         login_scr.keypad(False)
@@ -70,24 +77,32 @@ class Tui:
 
     def logo_print(self, scr):
         with open(path.join('ascii_art', 'logo.txt')) as logo_file:
-            logo_count = 7
+            logo_count = int(self.scr_size[0] - 27)
             for line in logo_file:
-                scr.addstr(logo_count, 12, line)
+                scr.addstr(logo_count, int(self.scr_size[1] - 30), line)
                 logo_count += 1
                 scr.refresh()
                 time.sleep(0.05)
 
     def auth_api(self, auth_scr):
-        auth_scr.addstr(10, 40, 'Authenticating your API token.')
+        auth_scr.addstr(int(self.scr_size[0] - 8), 
+                        int(self.scr_size[1] -15),
+                        'Authenticating your API token.')
         try:
             a = self.code_wars.train_next_code_challenge('python', peek=True)
         except HTTPError:
-            auth_scr.addstr(11,41, 'Authentication failed. Gtfo.')
-            auth_scr.addstr(18,42, 'Press the Any key to exit.')
+            auth_scr.addstr(int(self.scr_size[0] - 7), 
+                            int(self.scr_size[1] -14),
+                            'Authentication failed. Gtfo.')
+            auth_scr.addstr(int(self.scr_size[0] - 4), 
+                            int(self.scr_size[1] -13),
+                            'Press the Any key to exit.')
             auth_scr.refresh()
             auth_scr.getkey()
         else:
-            auth_scr.addstr(11,47, 'Authenticated.')
+            auth_scr.addstr(int(self.scr_size[0] - 7), 
+                            int(self.scr_size[1] -7),
+                            'Authenticated.')
             auth_scr.refresh()
             time.sleep(3)
 
