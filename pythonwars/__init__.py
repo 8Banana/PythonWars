625bd92f848572e0455774d43c83dc9f87b98ab8
ColorfulBanana <therealrebuilder@gmail.com>
2016-07-30 03:40:51 +0200

    Add module docstring

--- pythonwars/__init__.py
+++ pythonwars/__init__.py
@@ -1,2 +1,8 @@
-#!/usr/bin/env python3
+"""
+A module for dealing with the CodeWars API.
+The CodeWars class can be initialized with an API key to handle various API
+calls.
+More information about the API:
+    http://dev.codewars.com/
+"""
 from .pythonwars import CodeWars
